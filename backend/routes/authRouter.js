76739a1b86ea0ed82f4700309d834801c550b4be
August <76185140+Augustina1995@users.noreply.github.com>
2024-07-27 20:22:55 +0300

    starting again

--- backend/routes/authRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-router.post("/github")
-router.post("/facebook")
-
-export default router;
--- backend/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const router = express.Router();
-
-router.get("/profile");
-router.delete("/reservation/:id")
-
-export default router;
