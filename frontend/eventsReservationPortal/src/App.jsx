4cd367ce15e5ac11d193176caad687054c993c88
August <76185140+Augustina1995@users.noreply.github.com>
2024-07-27 21:54:52 +0300

    extra frontend dependencies installed, Home page file created

--- frontend/eventsReservationPortal/src/App.jsx
+++ frontend/eventsReservationPortal/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
